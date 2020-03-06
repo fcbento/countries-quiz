260097b4f3b975bac93bb6db307046511e692040
Felipe Costa <felipe.costa@resourceit.com>
2020-03-06 16:34:12 -0300

    #TASK-003 - Quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -7,19 +7,35 @@ import { CountriesService } from '../services/countries.service';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-  
-  public questions: number = 10;
-    
+
+  public questions;
+  public country;
+  public current: number = 1;
+
   constructor(private countryService: CountriesService) { }
 
   ngOnInit() {
-    //this.countryService.getCountries().subscribe(res => console.log(res))
-    this.question();
+
+    this.countryService.getCountries().subscribe((res) => {
+      this.questions = res;
+      this.getRandomCountry(this.questions);
+    });
+
   }
 
-  public question(){
-    let i: number = 0;
-
+  getRandomCountry(arr: []) {
+    this.country = arr[Math.floor(Math.random() * arr.length)];
   }
 
+  goToNextQuestion() {
+    this.current += 1;
+    if (this.current === 11) {
+      this.current = 11;
+    }
+    this.getRandomCountry(this.questions);
+  }
+
+  finishQuiz(){
+    console.log('finish')
+  }
 }
