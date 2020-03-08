f936a61522a1fc41bdd27f388168777487176fc3
Felipe Bento <felipe.16costa@gmail.com>
2020-03-08 11:29:12 -0300

    #TASK-003 - Quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,6 +11,8 @@ export class QuizComponent implements OnInit {
   public questions;
   public country;
   public current: number = 1;
+  public answer: string = '';
+  public rightAnswers = [];
 
   constructor(private countryService: CountriesService) { }
 
@@ -24,18 +26,28 @@ export class QuizComponent implements OnInit {
   }
 
   getRandomCountry(arr: []) {
-    this.country = arr[Math.floor(Math.random() * arr.length)];
+    this.country = arr[Math.floor(Math.random() * arr.length + 1)];
   }
 
   goToNextQuestion() {
+    this.checkAnswer(this.answer, this.country['name']);
     this.current += 1;
-    if (this.current === 11) {
-      this.current = 11;
-    }
+    this.answer = '';
     this.getRandomCountry(this.questions);
   }
 
-  finishQuiz(){
-    console.log('finish')
+
+  finishQuiz() {
+    this.checkAnswer(this.answer.toUpperCase(), this.country['name'].toUpperCase())
+  }
+
+  checkAnswer(answer: string, rightAnswer: string) {
+   
+    let ans = answer.toUpperCase()      
+    let rta = rightAnswer.toUpperCase();
+
+    if (rta.includes(ans)) {
+      this.rightAnswers.push(rightAnswer);
+    }
   }
 }
